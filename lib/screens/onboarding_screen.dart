6e85ea198ce46ecc18acc34d1470b781a1975238
Odarteifx <williamlamp512@gmail.com>
2025-02-05 11:32:05 +0000

    Ai Assistant App birthed :)

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AiOnboarding extends StatelessWidget {
+  const AiOnboarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Hello World!'),),
+    );
+  }
+}
