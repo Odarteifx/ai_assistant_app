29a6cc030d502a34f5952a31497307ed8ed4ba45
Odarteifx <williamlamp512@gmail.com>
2025-02-06 18:22:56 +0000

    Firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:ai_assistant_app/constants/colors.dart';
 import 'package:ai_assistant_app/router.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 import 'screens/onboarding_screen.dart';
 
-void main() {
+late final FirebaseApp app;
+late final FirebaseAuth auth;
+void main()  async{
+  WidgetsFlutterBinding.ensureInitialized;
   runApp(const MyApp());
 }
 
