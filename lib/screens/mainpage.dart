3e80313713c9ead17f517b02bb2d070ea3d122fc
Odarteifx <williamlamp512@gmail.com>
2025-02-07 10:16:06 +0000

    Chatpage Ui

--- lib/screens/mainpage.dart
+++ lib/screens/mainpage.dart
@@ -1,12 +1,38 @@
+import 'package:ai_assistant_app/constants/typography.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 class Mainpage extends StatelessWidget {
   const Mainpage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-    body: Center(child: Text('data')),
+    return  Scaffold(
+    body: SafeArea(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ShadButton.ghost(
+                icon: Icon(LucideIcons.menu, size: 20.sp,),
+                onPressed: () {
+                },
+              ),
+               Text('New Chat', style: TextStyle(fontSize: 16.sp),),
+               ShadButton.ghost(
+                icon: Icon(LucideIcons.messageSquarePlus, size: 20.sp,),
+                onPressed: () {
+                  
+                },
+              )
+
+            ],
+          )
+        ],
+      ),
+    )
     );
   }
 }
--- lib/screens/singin_screen.dart
+++ lib/screens/singin_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:ai_assistant_app/constants/ai_assets.dart';
 import 'package:ai_assistant_app/constants/colors.dart';
 import 'package:ai_assistant_app/constants/typography.dart';
